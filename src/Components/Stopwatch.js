f06e9866d4bd0b5fe697eeb32ecc9c77b99749fe
Vikrampaswan07 <130029534+Vikrampaswan07@users.noreply.github.com>
2024-07-15 12:43:14 +0530

    stopwatch

---
+++ src/Components/Stopwatch.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+const Stopwatch = () => {
+    var showdate = new Date();
+    var dt = showdate.toDateString();
+    var displaytime = showdate.getHours()+':'+showdate.getMinutes()+':'+showdate.getSeconds();
+
+
+    const [time, setTime] = useState({hr:0, min:0, sec:0, milli:0 })
+    const [status, setStatus] = useState()
+    let updHr = time.hr;
+    let updMin = time.min;
+    let updSec = time.sec;
+    let updMilli = time.milli;
+
+    const start = () =>{
+        myFun();
+        setStatus(setInterval(myFun, 10))
+    }
+    const stop = () => {
+        clearInterval(status);
+    }
+    const reset = () => {
+        clearInterval(status);
+        setTime({hr:0, min:0, sec:0, milli:0 })
+    }
+    const myFun = () =>{
+        if(updMilli === 100){
+            updMilli=0;
+            updSec++;
+        }
+        if(updSec === 60){
+            updSec=0;
+            updMin++;
+        }
+        if(updMin === 60){
+            updMin=0;
+            updHr++;
+        }
+        updMilli++;
+        return setTime({hr:updHr, min:updMin, sec:updSec, milli:updMilli })
+    }
+  return (
+    <>
+        <div className='container'>
+
+            <div className='datetime'>
+                <center>
+                {dt} - {displaytime}
+                </center>
+            </div>
+
+
+            <h1>{time.hr+" : "+time.min+" : "+time.sec+" : "+time.milli}</h1>
+            <div className='buttons'>
+                <button className='start' onClick={start}>Start</button>
+                <button className='stop' onClick={stop}>Stop</button>
+                <button className='reset' onClick={reset}>Reset</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Stopwatch
